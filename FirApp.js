5914a45c559b81062a38bbc9707cc89adf4259ed
nayema laboni <nayema.laboni53@myhunter.cuny.edu>
2020-01-27 21:54:50 -0500

    First commit

---
+++ FirApp.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// initialize our express app
+const product = require('./routes/product.route'); // Imports routes for the products
+
+const app = express();
+app.use('/products', product);
+
+let port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
+});
